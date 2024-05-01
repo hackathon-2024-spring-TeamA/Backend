1b536c3dd4a004cfbec02ec017d3d022c6cfd3ef
hiroki <hiroki.hkt@gmail.com>
2024-05-01 21:51:58 +0900

    ディレクトリ構成を機能ごとに変更する

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,5 +1,26 @@
-from app.schemas.types import type_defs as types_type_defs
-from app.schemas.queries import type_defs as queries_type_defs
-from app.schemas.mutations import type_defs as mutations_type_defs
+from app.schemas.user.types import type_defs as types_type_defs
+from app.schemas.user.queries import type_defs as queries_type_defs
+from app.schemas.user.mutations import type_defs as mutations_type_defs
 
 schema_type_defs = [types_type_defs, queries_type_defs, mutations_type_defs]
+
+# 今後機能が増えたら以下のようにまとめることで対応できる
+
+# from app.schemas.user.types import type_defs as user_types_type_defs
+# from app.schemas.user.queries import type_defs as user_queries_type_defs
+# from app.schemas.user.mutations import type_defs as user_mutations_type_defs
+
+# # 他の機能のtype_defsをインポート
+# from app.schemas.book.types import type_defs as book_types_type_defs
+# from app.schemas.book.queries import type_defs as book_queries_type_defs
+# from app.schemas.book.mutations import type_defs as book_mutations_type_defs
+
+# schema_type_defs = [
+#     user_types_type_defs,
+#     user_queries_type_defs,
+#     user_mutations_type_defs,
+#     book_types_type_defs,
+#     book_queries_type_defs,
+#     book_mutations_type_defs,
+#     # 他の機能のtype_defsを追加
+# ]
