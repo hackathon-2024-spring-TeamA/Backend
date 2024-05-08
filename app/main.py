f2c6ca5fc56e74a3a9ca2522af5572bc625e4e0a
hiroki <hiroki.hkt@gmail.com>
2024-05-08 23:28:27 +0900

    middlewareでCORS設定、[localhost:5173]を許可

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from ariadne import make_executable_schema
 from ariadne.asgi import GraphQL
 from app.resolvers import query, mutation# from app.resolvers.mutations import mutation
@@ -6,6 +7,15 @@ from app.schemas import schema_type_defs
 from app.db import create_context
 
 app = FastAPI()
+
+# CORSの設定を追加
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 schema = make_executable_schema(schema_type_defs, query + mutation)
 graphql_app = GraphQL(schema, debug=True, context_value=create_context)
