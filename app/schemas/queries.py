90ce0743107f95c6c54d339ce12534018c21824e
hiroki <hiroki.hkt@gmail.com>
2024-05-10 16:18:14 +0900

    [mod]QueryとMutationのスキーマ定義にコメントを追加

--- app/schemas/queries.py
+++ app/schemas/queries.py
@@ -1,6 +1,9 @@
 type_defs = """
     type Query {
-        paginatedBookRequests(page: Int!, perPage: Int!): PaginationData!
+        ### [Mock] ###
         authenticateUser(request: Request!): Response
+
+        ### [リクエスト機能] ###
+        paginatedBookRequests(page: Int!, perPage: Int!): PaginationData!
     }
 """
